7be70f1aae86ed097c444ab66cfaa2a5c2a2a5f4
Gustavo Diosa <elkyndiosa23@gmail.com>
2020-10-08 07:33:53 -0500

    Modificaciones generales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,37 +3,51 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" user-scalable=no>
+    {{-- OPTIMIZE SEO -  GUSTAVO DIOSA AUTHOR --}}
+    <title>Divinas Prepagos</title>
+    <meta name="title" content="Prepagos en Medellín - DivinasPrepagos" />
+	<meta name="description" content="Prepagos en medellin, ofrecemos las mejores escorts del valle de aburra, contrata una prepago y obten el mejor sexo de tu vida." />
+    <meta name="robots" content="index, follow" />
+    <meta name="keywords" content="Prepagos medellin, Escorts y prepagos en Medellin, Sexo en medellin"/>
+    <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+    <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+    <meta name="geo.country" content="CO"/>
+    <meta name="geo.placename" content="Medellín"/>
+    <meta name="geo.regioncode" content="CO-ANT"/>
+    <meta name="geo.position" content="6.2530408;-75.5645737"/>
+    <link rel="alternate" hreflang="es" href="https://divinasprepagos.com/" />
+    <link rel="alternate" hreflang="x-default" href="https://divinasprepagos.com/" />
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
+    <link rel="apple-touch-icon" href="{{ asset('images/favicon.png') }}" />
+    <meta name="msapplication-TileColor" content="#FFFFFF"/>
+	<link rel="canonical" href="https://divinasprepagos.com/" />
+	<meta property="og:locale" content="es_ES" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Prepagos en Medellín | DivinasPrepagos" />
+	<meta property="og:description" content="Prepagos de medellin en un unico lugar, ofrecemos las mejores prapagos del valle de aburra, contrata una prepago y obten el mejor sexo de tu vida." />
+    <meta property="og:image" content="https://divinasprepagos.com/images/logo-without.png" />
+    <meta property="og:url" content="https://divinasprepagos.com/" />
+	<meta property="og:site_name" content="Divinas Prepagos" />
+	<meta property="article:publisher" content="https://www.facebook.com/Divinas-Prepagos-109849570873366/" />
+	<meta property="article:modified_time" content="2020-07-24T15:10:31+00:00" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:creator" content="" />
+    <meta name="twitter:site" content="" />
+    <meta name="twitter:title" contnet="DivinasPrepagos">
+    <meta name="twitter:description" content="Prepagos de medellin en un unico lugar, ofrecemos las mejores prapagos del valle de aburra, contrata una prepago y obten el mejor sexo de tu vida.">
+    <meta name="twitter:image:alt" content="DivinasPrepagos">
+    <meta name="twitter:site" content="@website-username">
+
+    {{-- <script type="application/ld+json">{"@context":"https://schema.org","@graph":[{"@type":"Organization","@id":"https://divinasprepagos.com/#organization","name":"Divina Prepagos","url":"https://divinasprepagos.com/","sameAs":["https://www.facebook.com/Divinas-Prepagos-109849570873366/","https://www.instagram.com/divinasprepagos/?hl=af"],"logo":{"@type":"ImageObject","@id":"https://www.divinasprepagos.com.co/#logo","inLanguage":"es","url":"https://wwww.divinasprepagos.com/images/logo.png","width":4500,"height":4500,"caption":"Divinas Prepagos"},"image":{"@id":"https://wwww.divinasprepagos.com/images/logo.png"}},{"@type":"WebSite","@id":"https://www.divinasprepagos.com.co/#website","url":"https://wwww.divinasprepagos.com/images/logo.png","name":"Divinas Prepagos","description":"Sexo en medellin","publisher":{"@id":"https://www.divinasprepagos.com.co/#organization"},"potentialAction":[{"@type":"SearchAction","target":"https://www.divinasprepagos.com.co/?s={search_term_string}","query-input":"required name=search_term_string"}],"inLanguage":"es"},{"@type":"WebPage","@id":"https://www.divinasprepagos.com/#webpage","url":"https://www.divinasprepagos.com/","name":"Divinas Prepagos","isPartOf":{"@id":"https://www.divinasprepagos.com/#website"},"about":{"@id":"https://www.divinasprepagos.com/#organization"},"datePublished":"2020-05-03T22:55:51+00:00","dateModified":"2020-07-24T15:10:31+00:00","description":"Venta de insumos medicos y equipos de proteccion personal en medellin, Cuidarnos del coronavirus a traves de nuestra tienda medica.","inLanguage":"es","potentialAction":[{"@type":"ReadAction","target":["https://www.protegt.com.co/"]}]}]}</script> --}}
+    {{-- END OPTIMIZE SEO --}}
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="user" content="{{ Auth::user() }}">
     {{-- FAVICON --}}
-    {{-- <link rel="icon" type="image/png" href="" /> --}}
     <link href="https://fonts.googleapis.com/css?family=Material+Icons" rel="stylesheet">
-
-    <title>divinasprepagos.com</title>
-
-        <!--  Essential META Tags Socials -->
-        <meta property="og:title" content="Divinas Prepagos">
-        <meta property="og:description" content="Prepagos Medellin">
-        {{-- <meta property="og:image" content="" /> --}}
-        {{-- <meta property="og:url" content=""> --}}
-        <meta property="og:site_name" content="DivinasPrepagos">
-        <meta name="twitter:card" content="summary_large_image">
-        {{-- <meta name="twitter:url" contnet=""> --}}
-        <meta name="twitter:title" contnet="DivinasPrepagos">
-        <meta name="twitter:description" content="Prepagos Medellin">
-        <meta name="twitter:title" contnet="DivinasPrepagos">
-        {{-- <meta name="twitter:image" content=" "--}}
-        <meta name="twitter:image:alt" content="DivinasPrepagos">
-        <meta name="twitter:site" content="@website-username">
-        <meta property="og:url"                content="https://divinasprepagos.com/" />
-        <meta property="og:type"               content="web" />
-        <meta property="og:title"              content="DivinasPrepagos" />
-        <meta property="og:description"        content="Prepagos Medellin" />
-        <meta property="og:image"              content="https://divinasprepagos.com/images/logo-without.png" />
-
-        <link  rel="icon"   href="{{ asset('images/favicon.png') }}" type="image/png" />
+    <link  rel="icon"   href="{{ asset('images/favicon.png') }}" type="image/png" />
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
